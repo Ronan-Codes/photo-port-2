4413fdae99a199a0b1dd06644439c06884c0a542
Ronan-Codes <galv.ronan@gmail.com>
2021-08-22 12:58:49 -0400

    PhotoList in progress

--- src/App.js
+++ src/App.js
@@ -37,7 +37,9 @@ function App() {
       ></Nav>
       <main>
         <div>
-          <Gallery></Gallery>
+          <Gallery
+          currentCategory={currentCategory}
+          ></Gallery>
           <About></About>
         </div>
       </main>
